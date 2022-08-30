781ff9f9a9a9f2b3dd908187c1fe8ba6e61dfb47
Vikastiwari7744 <vikastiwari7744@gmail.com>
2022-08-30 15:09:36 +0530

    New Update

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8-fa4xzz4i23n@6^=vxjn85h#%ed)*dwt0e#ybss7$+(b-ipkt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
